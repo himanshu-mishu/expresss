3004815ad0b4f36810d8782ffd5fa3e9edde2531
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-05-29 08:17:18 +0530

    five

--- myApp.js
+++ myApp.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 
-// Middleware to serve static files from /public
-app.use("/public", express.static(__dirname + "/public"));
-
-// Route to serve HTML file at '/'
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+// JSON API endpoint
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
