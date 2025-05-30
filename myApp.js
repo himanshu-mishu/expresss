641e14f599cd5f64872d5a6abc3621b2b0b37d23
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-05-30 06:29:33 +0530

    env

--- myApp.js
+++ myApp.js
@@ -1,20 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 // ✅ Load environment variables
-require("dotenv").config();
 
 app.get("/json", function (req, res) {
-  // default response
   let response = "Hello json";
-
-  // ✅ Conditionally convert to uppercase
   if (process.env.MESSAGE_STYLE === "uppercase") {
     response = response.toUpperCase();
   }
-
-  // ✅ Send as JSON
   res.json({ message: response });
 });
-
 module.exports = app;
