cdcda518b22f801f01bb12b81c284fc129c58fae
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-05-30 07:33:09 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ const app = express();
 
 require("dotenv").config();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/json", function (req, res) {
   let response = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
